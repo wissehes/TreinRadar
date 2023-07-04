93e601ff1799297f99346e9cdbb2650ba9626953
Wisse <wissehes@gmail.com>
2023-07-04 21:35:39 +0200

    Use new way of formatting distances + fix bug where nearby stations would not appear

--- TreinRadar/Views/StationsSearchView/StationsSearchViewModel.swift
+++ TreinRadar/Views/StationsSearchView/StationsSearchViewModel.swift
@@ -8,6 +8,16 @@
 import Foundation
 import CoreLocation
 
+extension MeasurementFormatter {
+    static var myFormatter: MeasurementFormatter {
+        let formatter = self.init()
+        formatter.unitOptions = [.providedUnit, .naturalScale]
+        formatter.unitStyle = .long
+        formatter.numberFormatter.maximumFractionDigits = 0
+        return formatter
+    }
+}
+
 struct StationWithDistance: Station {
     var code: String
     var name: String
@@ -16,12 +26,16 @@ struct StationWithDistance: Station {
     var distance: Double
     
     func fDistance() -> String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 0
-        let number = NSNumber(value: self.distance)
+        let distanceInMeters = Measurement(value: self.distance, unit: UnitLength.meters)
+        let formatter = MeasurementFormatter.myFormatter
         
-        return formatter.string(from: number) ?? self.distance.formatted()
+        return formatter.string(from: distanceInMeters)
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .decimal
+//        formatter.maximumFractionDigits = 0
+//        let number = NSNumber(value: self.distance)
+//        
+//        return formatter.string(from: number) ?? self.distance.formatted()
     }
     
     init(_ station: FullStation, location: CLLocation, distance: Double) {
@@ -34,24 +48,43 @@ struct StationWithDistance: Station {
 }
 
 final class StationsSearchViewModel: ObservableObject {
+    
+    @Published var nearbyStations: [StationWithDistance]?
+    
     init() {
         LocationManager.shared.requestLocation()
     }
     
-    var nearbyStations: [StationWithDistance]? {
-        guard let stations = StationsManager.shared.stations else { return nil }
-        guard let location = LocationManager.shared.location else { return nil }
-        let me = CLLocation(latitude: location.latitude, longitude: location.longitude)
-
+    func getNearbyStations() {
+        guard let stations = StationsManager.shared.stations else { return }
+        guard let location = LocationManager.shared.location else { return }
+        
         let mapped: [StationWithDistance] = stations.map { station in
             let stationLocation = CLLocation(latitude: station.lat, longitude: station.lng)
-            let distance = me.distance(from: stationLocation)
+            let distance = location.distance(from: stationLocation)
             
             return .init(station, location: stationLocation, distance: distance)
         }
         
         let sorted = mapped.sorted { $0.distance < $1.distance }
         
-        return Array(sorted.prefix(5))
+        self.nearbyStations = Array(sorted.prefix(5))
     }
+    
+//    var nearbyStations: [StationWithDistance]? {
+//        guard let stations = StationsManager.shared.stations else { return nil }
+//        guard let location = LocationManager.shared.location else { return nil }
+//        let me = CLLocation(latitude: location.latitude, longitude: location.longitude)
+//
+//        let mapped: [StationWithDistance] = stations.map { station in
+//            let stationLocation = CLLocation(latitude: station.lat, longitude: station.lng)
+//            let distance = me.distance(from: stationLocation)
+//            
+//            return .init(station, location: stationLocation, distance: distance)
+//        }
+//        
+//        let sorted = mapped.sorted { $0.distance < $1.distance }
+//        
+//        return Array(sorted.prefix(5))
+//    }
 }
