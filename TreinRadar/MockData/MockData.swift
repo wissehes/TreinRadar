0338283a9add851574f8c641fd385b6f354fa758
Wisse <wissehes@gmail.com>
2023-07-05 13:30:55 +0200

    Add some mock data for previews

---
+++ TreinRadar/MockData/MockData.swift
@@ -0,0 +1,40 @@
+//
+//  MockData.swift
+//  TreinRadar
+//
+//  Created by Wisse Hes on 05/07/2023.
+//
+
+import Foundation
+
+final class MockData {
+    
+    let decoder: JSONDecoder
+    
+    enum MockDataError: Error {
+        case resourceError
+        case dataError
+        case decodingError
+    }
+    
+    
+    init() {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        self.decoder = decoder
+    }
+    
+    func getData<T>(resource: String, type: T.Type) throws -> T where T : Decodable {
+        guard let path = Bundle.main.path(forResource: resource, ofType: "json") else {
+            throw MockDataError.resourceError
+        }
+        
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe) else {
+            throw MockDataError.dataError
+        }
+        guard let decoded = try? self.decoder.decode(type.self, from: data) else {
+            throw MockDataError.decodingError
+        }
+        return decoded
+    }
+}
