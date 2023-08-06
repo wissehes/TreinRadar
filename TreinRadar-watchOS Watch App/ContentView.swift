7740ae936f6ebe0ce54695317dbea95be5f00a45
Wisse <wissehes@gmail.com>
2023-08-06 13:39:21 +0200

    Initialize watchOS app

---
+++ TreinRadar-watchOS Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TreinRadar-watchOS Watch App
+//
+//  Created by Wisse Hes on 06/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
