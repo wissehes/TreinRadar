673d8214e4f2e847fc90b10d520c002d9d3af488
Wisse <wissehes@gmail.com>
2023-08-13 21:48:27 +0200

    Add some mock data

---
+++ TreinRadar-watchOS/Views/LookupTrain/JourneyStopItem.swift
@@ -0,0 +1,18 @@
+//
+//  JourneyStopItem.swift
+//  TreinRadar-watchOS
+//
+//  Created by Wisse Hes on 13/08/2023.
+//
+
+import SwiftUI
+
+struct JourneyStopItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JourneyStopItem()
+}
---
+++ TreinRadar-watchOS/Views/NearbyTrains/NearbyTrainItem.swift
@@ -0,0 +1,18 @@
+//
+//  NearbyTrainItem.swift
+//  TreinRadar-watchOS
+//
+//  Created by Wisse Hes on 13/08/2023.
+//
+
+import SwiftUI
+
+struct NearbyTrainItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NearbyTrainItem()
+}
