831f2c2e24825d617141d6e2537de3ae7d053279
Wisse <wissehes@gmail.com>
2023-09-13 22:20:20 +0200

    Create station view

--- TreinRadar-iOS/Views/Station/StationView.swift
+++ TreinRadar-iOS/Views/Station/StationView.swift
@@ -7,12 +7,77 @@
 
 import SwiftUI
 
+/**
+ Station overview
+ - Station details (tracks, station type, etc)
+ - Most recent departures
+ - Most recent arrivals
+ */
 struct StationView: View {
+    
+    var station: FullStation
+    @StateObject var vm = StationViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("Vertrektijden") {
+                    if let departures = vm.departures {
+                        ForEach(departures, id: \.name, content: departureItem(item:))
+                        
+                        NavigationLink("Meer", value: station)
+                            .foregroundStyle(Color.accentColor)
+                    } else {
+                        HStack {
+                            Spacer()
+                            ProgressView()
+                            Spacer()
+                        }
+                    }
+                }
+                
+                Section("Aankomsttijden") {
+                    Text("vertrek 1")
+                    Text("vertrek 2")
+                }
+            }.navigationTitle(station.namen.lang)
+                .navigationDestination(for: FullStation.self, destination: { station in
+                    DeparturesView(station: station)
+                })
+                .headerProminence(.increased)
+                .task {
+                    await vm.fetchDepartures(station)
+                }
+                .refreshable {
+                    await vm.fetchDepartures(station)
+                }
+        }
+    }
+    
+    func departureItem(item: Departure) -> some View {
+        HStack(alignment: .center) {
+            VStack(alignment: .leading) {
+                HStack(alignment: .center) {
+                    
+                    Text(item.actualDateTime, style: .time)
+                        .bold()
+                    
+                    Text(item.product.longCategoryName)
+                        .font(.subheadline)
+                    
+                }
+                
+                Text(item.direction)
+            }
+            
+            if let platform = item.actualTrack {
+                Spacer()
+                PlatformIcon(platform: platform)
+            }
+        }
     }
 }
 
 #Preview {
-    StationView()
+    StationView(station: try! MockData().getData(resource: "station", type: FullStation.self))
 }
--- TreinRadar-iOS/Views/Station/StationViewModel.swift
+++ TreinRadar-iOS/Views/Station/StationViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+final class StationViewModel: ObservableObject {
+    
+    @Published var departures: [Departure]?
+//    @Published var arrivals:
+    
+    func fetchDepartures(_ station: any Station) async {
+        do {
+            let departures = try await API.shared.getDepartures(stationCode: station.code)
+            let slice = Array(departures.prefix(3))
+            DispatchQueue.main.async {
+                withAnimation { self.departures = slice }
+            }
+        } catch { print(error) }
+    }
+}
