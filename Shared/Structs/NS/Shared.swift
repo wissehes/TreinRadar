392c0fa27d4c781eeae9eff972f5b75099d3c949
Wisse <wissehes@gmail.com>
2023-09-17 16:40:03 +0200

    Update shared structs

--- Shared/Structs/NS/Shared.swift
+++ Shared/Structs/NS/Shared.swift
@@ -9,9 +9,36 @@ import Foundation
 
 // MARK: Product
 struct Product: Codable, Hashable {
+    
+    enum ModelType: String, Codable {
+        case train = "TRAIN"
+        case bus = "BUS"
+        case tram = "TRAM"
+        case metro = "METRO"
+        case ferry = "FERRY"
+        case walk = "WALK"
+        case bike = "BIKE"
+        case car = "CAR"
+        case taxi = "TAXI"
+        case subway = "SUBWAY"
+        case sharedModality = "SHARED_MODALITY"
+        case unknown = "UNKNOWN"
+    }
+    
     let number: String
     let categoryCode: String
     let shortCategoryName, longCategoryName: String
     let operatorCode, operatorName: String
-    let type: String
+    let type: ModelType
+}
+
+// MARK: - Message
+struct Message: Codable, Hashable {
+    let message: String
+    let style: MessageStyle
+}
+
+enum MessageStyle: String, Codable, CaseIterable {
+    case info = "INFO"
+    case warning = "WARNING"
 }
