b8935a26eb673c7ced33bc6ff73e0eda902184e9
Wisse <wissehes@gmail.com>
2023-09-24 14:26:31 +0200

    Create new JourneyMapView

--- TreinRadar-iOS/Views/Map/JourneyMap/JourneyMapView.swift
+++ TreinRadar-iOS/Views/Map/JourneyMap/JourneyMapView.swift
@@ -6,13 +6,60 @@
 //
 
 import SwiftUI
+import MapKit
 
+@available(iOS 17.0, *)
 struct JourneyMapView: View {
+    
+    var data: StopsAndGeometry
+    var liveTrain: LiveTrain?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map {
+            MapPolyline(coordinates: data.actualCoordinates)
+                .stroke(.blue, lineWidth: 3)
+            
+            ForEach(data.stops, id: \.uicCode) { stop in
+                Marker(stop.name, systemImage: "tram", coordinate: stop.coordinates)
+                    .foregroundStyle(.blue)
+            }
+            
+            trainMarker
+            
+        }.mapStyle(.standard(pointsOfInterest: [.publicTransport]))
+    }
+    
+    @MapContentBuilder
+    var trainMarker: some MapContent {
+        if let live = liveTrain, let image = live.image {
+            Annotation("\(live.speed) km/h", coordinate: live.location) {
+                AsyncImage(url: URL(string: image)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    EmptyView()
+                }.frame(width: 50, height: 25)
+
+            }
+        }
     }
 }
 
 #Preview {
-    JourneyMapView()
+    if #available(iOS 17.0, *) {
+        JourneyMapView(
+            data: try! MockData().getData(resource: "stops-geometry", type: StopsAndGeometry.self), 
+            liveTrain: .init(
+                latitude: 52.5,
+                longitude: 5,
+                speed: 20,
+                direction: 50,
+                track: nil,
+                image: "https://trein.wissehes.nl/api/image/DDZ%204"
+            )
+        )
+    } else {
+        EmptyView()
+    }
 }
