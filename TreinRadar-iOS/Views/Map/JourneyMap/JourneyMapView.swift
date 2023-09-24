70522e25c17226733adb56fa74dd2159adeeb6d6
Wisse <wissehes@gmail.com>
2023-09-24 14:25:50 +0200

    Add mock data

---
+++ TreinRadar-iOS/Views/Map/JourneyMap/JourneyMapView.swift
@@ -0,0 +1,18 @@
+//
+//  JourneyMapView.swift
+//  TreinRadar-iOS
+//
+//  Created by Wisse Hes on 24/09/2023.
+//
+
+import SwiftUI
+
+struct JourneyMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JourneyMapView()
+}
