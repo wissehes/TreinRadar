6afce0c3389149df10232f7b1449dc526e30060d
Wisse <wissehes@gmail.com>
2023-10-04 21:46:27 +0200

    Show loading indicator for nearby trains/stations (#22)

--- TreinRadar-iOS/Views/TravelInfoView/TravelInfoView.swift
+++ TreinRadar-iOS/Views/TravelInfoView/TravelInfoView.swift
@@ -17,16 +17,10 @@ struct TravelInfoView: View {
         NavigationStack(path: $vm.presentedJourneys) {
             List {
                 searchTrains
-                if let currentJourney = trainManager.currentJourney {
-                    trainDetection(currentJourney)
-                }
                 
-                if let nearbyStations = stationsManager.nearbyStations {
-                    Section("Stations in de buurt") {
-                        ForEach(nearbyStations.prefix(3), id: \.code) { station in
-                            NavigationLink(station.name, value: station)
-                        }
-                    }
+                if locationManager.permissionStatus == .authorizedWhenInUse {
+                    trainDetection
+                    nearbyStations
                 }
             }.navigationTitle("Reisinformatie")
                 .navigationDestination(for: JourneyId.self) { id in
@@ -35,6 +29,8 @@ struct TravelInfoView: View {
                 .navigationDestination(for: StationWithDistance.self) { station in
                     StationView(station: station)
                 }
+                .animation(.easeInOut, value: trainManager.currentJourney)
+                .animation(.easeInOut, value: stationsManager.nearbyStations)
                 .refreshable {
                     await trainManager.getData()
                     locationManager.requestLocation()
@@ -45,7 +41,7 @@ struct TravelInfoView: View {
                 } message: { error in
                     Text(error.responseCode?.description ?? error.localizedDescription)
                 }
-
+            
         }
     }
     
@@ -63,13 +59,40 @@ struct TravelInfoView: View {
         }
     }
     
-    func trainDetection(_ journey: JourneyPayload) -> some View {
+    var trainDetection: some View {
         Section("Treindetectie") {
-            NavigationLink(value: journey.productNumbers.first) {
-                CurrentTrainView(journey: journey, distance: trainManager.currentTrain?.distance)
+            if let journey = trainManager.currentJourney {
+                NavigationLink(value: journey.productNumbers.first) {
+                    CurrentTrainView(journey: journey, distance: trainManager.currentTrain?.distance)
+                }
+            } else {
+                HStack(alignment: .center) {
+                    Text("Trein detecteren...")
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                    ProgressView()
+                }
             }
         }
     }
+    
+    var nearbyStations: some View {
+        Section("Stations in de buurt") {
+            if let nearbyStations = stationsManager.nearbyStations {
+                ForEach(nearbyStations.prefix(3), id: \.code) { station in
+                    NavigationLink(station.name, value: station)
+                }
+            } else {
+                HStack(alignment: .center) {
+                    Text("Stations ophalen...")
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                    ProgressView()
+                }
+            }
+        }
+    }
+    
 }
 struct TravelInfoView_Previews: PreviewProvider {
     static var previews: some View {
