576578a2abf73e67a8b6787d1f35e5de8070117a
Wisse <wissehes@gmail.com>
2023-10-21 15:22:19 +0200

    Add JourneyLiveActivitySettings

--- Shared/Structs/SavedJourney.swift
+++ Shared/Structs/SavedJourney.swift
@@ -25,6 +25,20 @@ enum SavedJourneyError: LocalizedError {
     }
 }
 
+/**
+ Configuration for following a journey with a live activity
+ */
+struct JourneyLiveActivitySettings: Codable, Hashable {
+    let startStation: StopInfo
+    let endStation: StopInfo
+    
+    /// Whether to notifiy the user for each stop
+    let notifyForEachStop: Bool
+    
+    /// Amount of minutes before arrival to send a notification
+    let arrivalNotification: Int
+}
+
 
 struct SavedJourney: Codable, Identifiable, Defaults.Serializable, Journey, Hashable {
     var id: String {
@@ -57,7 +71,9 @@ struct SavedJourney: Codable, Identifiable, Defaults.Serializable, Journey, Hash
     /// Product info
     let product: Product?
     
-    init(code: String, created: Date, start: StopInfo, startDate: Date?, end: StopInfo, endDate: Date?, product: Product?) {
+    let liveActivity: JourneyLiveActivitySettings?
+    
+    init(code: String, created: Date, start: StopInfo, startDate: Date?, end: StopInfo, endDate: Date?, product: Product?, liveActivity: JourneyLiveActivitySettings? = nil) {
         self.code = code
         self.saved = created
         self.start = start
@@ -65,9 +81,10 @@ struct SavedJourney: Codable, Identifiable, Defaults.Serializable, Journey, Hash
         self.end = end
         self.endDate = endDate
         self.product = product
+        self.liveActivity = liveActivity
     }
     
-    init(journey: JourneyPayload) throws {
+    init(journey: JourneyPayload, liveActivity: JourneyLiveActivitySettings? = nil) throws {
         guard let code = journey.productNumbers.first else { throw SavedJourneyError.noJourneyCode }
         self.code = code
         self.saved = .now
@@ -81,6 +98,7 @@ struct SavedJourney: Codable, Identifiable, Defaults.Serializable, Journey, Hash
         self.endDate = journey.stops.last?.arrival?.plannedTime
         
         self.product = journey.product
+        self.liveActivity = liveActivity
     }
     
 }
