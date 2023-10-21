4c39b5c8b5d00ab602b7f277112f3466c92c1328
Wisse <wissehes@gmail.com>
2023-10-21 15:37:32 +0200

    Create live activity file

--- TreinRadarWidgets/JourneyLiveActivity.swift
+++ TreinRadarWidgets/JourneyLiveActivity.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct JourneyLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for:  JourneyAttributes.self) { context in
+            VStack {
+                Text("Huidig/volgend station: \(context.state.journey.currentOrNextStop?.stop.name ?? "?")")
+            }
+        } dynamicIsland: { context in
+            DynamicIsland {
+                DynamicIslandExpandedRegion(.bottom) {
+                    //                Text(context.state.journey)
+                    Text("Hello!")
+                    // more content
+                }
+            }compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T")
+            } minimal: {
+                Text("M")
+            }.keylineTint(Color.red)
+        }
+    }
+}
